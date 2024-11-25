d77af68f60992e48178bcca2e768ed59755e5036
choiseyeon <choi20014830@gmail.com>
2024-11-26 00:08:02 +0900

    feat: medium 크롤러를 추가합니다.

--- dags/src/crawling/medium_crawler.py
+++ dags/src/crawling/medium_crawler.py
@@ -5,7 +5,8 @@ import uvloop
 import xmltodict
 import pandas as pd
 import re
-from io import BytesIO
+import uuid
+
 from datetime import datetime, timezone
 
 from dags.src.config.logger.logger_setup import setup_logging
@@ -24,6 +25,7 @@ class MediumCrawler:
             "watcha",
             "coupang-engineering"
         ]
+        self.filename = []
 
     async def fetch(self, url):
         async with aiohttp.ClientSession() as session:
@@ -36,34 +38,37 @@ class MediumCrawler:
     def create_rss_feed(self, company):
         return f"https://medium.com/feed/{company}"
 
-    async def crawl(self):
+    async def crawl(self, previous_datetime):
         tasks = [self.fetch(self.create_rss_feed(company)) for company in self.companies]
         results = await asyncio.gather(*tasks, return_exceptions=True)
         for result in results:
-            data = await self.parser(await self.clean_data(result), "2024-11-20T07:17:53.534Z")
+            data = await self.parser(await self.clean_data(result), previous_datetime)
             await self.to_parquet(data)
+
+        logger.debug(self.filename)
 
     async def clean_data(self, data):
         return re.sub(r'[^\x20-\x7E\uAC00-\uD7A3\u1100-\u11FF]', '', data)
 
     async def parser(self, data, previous_datetime):
-        last_data = []
+        data_list = []
         data = xmltodict.parse(data)
         items = data['rss']['channel']['item']
         if previous_datetime is not None:
             for item in items:
                 if item['atom:updated'] >= previous_datetime:
-                    last_data.append(item)
-            return last_data
+                    data_list.append(item)
+            return data_list
         if previous_datetime is None:
             return items
-
-        return last_data
+        return data_list
 
     async def to_parquet(self, data):
         df = pd.DataFrame(data)
         utc_date_string = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-        df.to_parquet(f"./datalake/data/{utc_date_string}/{}.parquet", engine="pyarrow", index=False)
+        random_text = uuid.uuid4()
+        df.to_parquet(f"./datalake/data/{utc_date_string}-{random_text}.parquet", engine="pyarrow", index=False)
+        self.filename.append(f'{utc_date_string}-{random_text}.parquet')
 
 if __name__ == '__main__':
-    asyncio.run(MediumCrawler().crawl())
+    asyncio.run(MediumCrawler().crawl(""))
